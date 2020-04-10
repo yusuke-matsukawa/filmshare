0324bb71bb473519ccd99eb1370f9cba285dbc2a
yusuke-matsukawa <yuumatsu8970@icloud.com>
2020-04-10 10:03:47 +0000

    [Add]投稿コメント表示機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    
+    post = Post.find(params[:post_id])
+    comment = current_user.comments.new(comment_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to request.referrer
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
