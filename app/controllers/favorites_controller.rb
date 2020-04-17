54b54dfb9c064217683a6014f32ce000b882d3ca
yusuke-matsukawa <yuumatsu8970@icloud.com>
2020-04-17 08:24:55 +0000

    [Add]いいね機能追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,14 @@
 class FavoritesController < ApplicationController
   def create
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: post.id)
+    favorite.save
+    redirect_to request.referrer
   end
   def destroy
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: post.id)
+    favorite.destroy
+    redirect_to request.referrer
   end
 end
